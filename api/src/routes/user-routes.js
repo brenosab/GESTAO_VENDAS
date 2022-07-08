98252d53c40d8395c25852e8eaddac727f6e4615
Breno <112519@pmenos.com.br>
2022-07-08 00:34:14 -0300

    routes modified

--- api/src/routes/user-routes.js
+++ api/src/routes/user-routes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user-controller');
 
+router.get('/info', userController.info);
 router.get('/', userController.listUsers);
 router.post('/', userController.createUser);
 router.get('/:id',userController.getUsers);
 router.get('/getUserLogin/:key?',userController.getUserLogin);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
-router.delete('/info', userController.info);
 
 module.exports = router;
